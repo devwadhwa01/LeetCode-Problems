a861fa8775a8e0f0538aa1ea796f52b17af4450a
Dev Wadhwa <73272997+devwadhwa01@users.noreply.github.com>
2021-08-13 00:49:32 +0530

    Create 69. Sqrt(x).cpp

---
+++ Solutions/69. Sqrt(x).cpp
@@ -0,0 +1,6 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        return floor(sqrt(x));          //sqrt function is used to find the square root of x and floor fuction is used to truncate the decimal digits
+    }
+};
