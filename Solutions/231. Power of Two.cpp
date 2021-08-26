94c35ae2d771d2195953bbccd19b4e1ea0acf624
Dev Wadhwa <73272997+devwadhwa01@users.noreply.github.com>
2021-08-26 15:07:26 +0530

    Update 231. Power of Two.cpp

--- Solutions/231. Power of Two.cpp
+++ Solutions/231. Power of Two.cpp
@@ -1,1 +1,18 @@
+//Dev Wadhwa
 
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n==1) return true;
+        while(n!=0)
+        {
+            if(n%2==0)
+            {
+                n/=2;
+            }
+            else return false;
+            if(n==1) return true;
+        }
+        return false;
+    }
+};
