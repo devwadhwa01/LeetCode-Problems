e83187f43937277b8173020826d3217675a77039
Dev Wadhwa <73272997+devwadhwa01@users.noreply.github.com>
2021-08-29 00:25:40 +0530

    Create 728. Self Dividing Numbers.cpp

---
+++ Solutions/728. Self Dividing Numbers.cpp
@@ -0,0 +1,23 @@
+//Dev Wadhwa
+
+class Solution {
+public:
+    vector<int> selfDividingNumbers(int left, int right) {
+        vector<int> result;
+        for(int i=left;i<=right;i++)
+        {
+            int n=i,flag=1;
+            while(n!=0)
+            {
+                int temp=n%10;
+                if(temp==0 || i%temp!=0)
+                {
+                    flag=0;
+                }
+                n/=10;
+            }
+            if(flag==1) result.push_back(i);
+        }
+        return result;
+    }
+};
