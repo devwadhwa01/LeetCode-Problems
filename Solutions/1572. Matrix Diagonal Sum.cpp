dd7a19c9bbfd8aebcac9b048aa684379f1d62b95
Dev Wadhwa <73272997+devwadhwa01@users.noreply.github.com>
2021-09-01 16:29:03 +0530

    Create 1572. Matrix Diagonal Sum.cpp

---
+++ Solutions/1572. Matrix Diagonal Sum.cpp
@@ -0,0 +1,16 @@
+//Dev Wadhwa
+
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int n=mat.size(),sum=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i==j || i+j==(n-1)) sum+=mat[i][j];
+            }
+        }
+        return sum;
+    }
+};
