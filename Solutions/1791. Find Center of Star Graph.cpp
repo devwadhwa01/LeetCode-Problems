fd45d5d8bc5968fb9e96d2f268a67a123f6f2853
Dev Wadhwa <73272997+devwadhwa01@users.noreply.github.com>
2021-09-24 09:11:34 +0530

    Update 1791. Find Center of Star Graph.cpp

--- Solutions/1791. Find Center of Star Graph.cpp
+++ Solutions/1791. Find Center of Star Graph.cpp
@@ -13,3 +13,13 @@ public:
         return temp;
     }
 };
+
+
+//Better Solution O(1) time and space
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& edges) {
+        if(edges[0][0]==edges[1][0] || edges[0][0]==edges[1][1]) return edges[0][0];
+        else return edges[0][1];
+    }
+};
