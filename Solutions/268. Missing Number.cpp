e51bc333d68f197bc636d023e42c13b42ef6074d
Dev Wadhwa <73272997+devwadhwa01@users.noreply.github.com>
2021-10-03 16:56:39 +0530

    Create 268. Missing Number.cpp

---
+++ Solutions/268. Missing Number.cpp
@@ -0,0 +1,14 @@
+//Dev Wadhwa
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]!=i) return i;
+        }
+        return n;
+    }
+};
