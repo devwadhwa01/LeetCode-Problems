36b8cf552f9df6dfa50bab6ee5c5b8cdccf65f60
Dev Wadhwa <73272997+devwadhwa01@users.noreply.github.com>
2021-10-15 01:09:00 +0530

    Create 344. Reverse String.cpp

---
+++ Solutions/344. Reverse String.cpp
@@ -0,0 +1,22 @@
+//Dev Wadhwa
+
+
+//Method 1
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int n=s.size();
+        for(int i=0;i<n/2;i++)
+        {
+            swap(s[i],s[n-i-1]);
+        }
+    }
+};
+
+//Method 2
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        reverse(s.begin(),s.end());
+    }
+};
