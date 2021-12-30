58f573676d717ca4786ac991bb9781e9f2213090
Dev Wadhwa <73272997+devwadhwa01@users.noreply.github.com>
2021-12-30 18:20:23 +0530

    Create 2057. Smallest Index With Equal Value.cpp

---
+++ Solutions/2057. Smallest Index With Equal Value.cpp
@@ -0,0 +1,13 @@
+//Dev Wadhwa
+
+class Solution {
+public:
+    int smallestEqual(vector<int>& nums) {
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            if((i%10)==nums[i]) return i;
+        }
+        return -1;
+    }
+};
