5bae2cd4f059a14074b5ce562e29e1dae11ac9aa
Dev Wadhwa <73272997+devwadhwa01@users.noreply.github.com>
2022-01-12 23:33:01 +0530

    Create 238. Product of Array Except Self.cpp

---
+++ Solutions/238. Product of Array Except Self.cpp
@@ -0,0 +1,43 @@
+//Dev Wadhwa
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> res;
+        int n=nums.size();
+        int pro=1;
+        int has0=0;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]==0)
+            {
+                has0++;
+                continue;
+            }
+            pro*=nums[i];
+        }
+        if(has0>1)
+        {
+            for(int i=0;i<n;i++)
+            {
+                res.push_back(0);
+            }
+        }
+        else if(has0==1)
+        {
+            for(int i=0;i<n;i++)
+            {
+                if(nums[i]==0) res.push_back(pro);
+                else res.push_back(0);
+            }
+        }
+        else
+        {
+            for(int i=0;i<n;i++)
+            {
+                res.push_back(pro/nums[i]);
+            }
+        }
+        return res;
+    }
+};
