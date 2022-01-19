787e78e72ff741f468696736b12d1684b8079ce9
Dev Wadhwa <73272997+devwadhwa01@users.noreply.github.com>
2022-01-19 23:14:26 +0530

    Update 1295. Find Numbers with Even Number of Digits.cpp

--- Solutions/1295. Find Numbers with Even Number of Digits.cpp
+++ Solutions/1295. Find Numbers with Even Number of Digits.cpp
@@ -17,3 +17,19 @@ public:
         return count;
     }
 };
+
+
+
+//Using log10 to find number of digits
+class Solution {
+public:
+    int findNumbers(vector<int>& nums) {
+        int n=nums.size(),count=0;
+        for(int i=0;i<n;i++)
+        {
+            int digit=floor(log10(nums[i])+1);
+            if(digit%2==0) count++;
+        }
+        return count;
+    }
+};
