a2d8ba642eb7b497418ac90786cd0fd11c436dc3
Dev Wadhwa <73272997+devwadhwa01@users.noreply.github.com>
2022-01-23 16:27:25 +0530

    Update 27. Remove Element.cpp

--- Solutions/27. Remove Element.cpp
+++ Solutions/27. Remove Element.cpp
@@ -27,7 +27,6 @@ public:
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int count = 0;
         for(int i=0;i<nums.size();i++){
             if(nums[i] == val){
                 count++;
