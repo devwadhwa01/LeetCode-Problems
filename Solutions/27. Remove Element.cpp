525bc6ac10bacee1c68e34fb7cd993bc63f31797
Dev Wadhwa <73272997+devwadhwa01@users.noreply.github.com>
2022-01-23 16:25:03 +0530

    Update 27. Remove Element.cpp

--- Solutions/27. Remove Element.cpp
+++ Solutions/27. Remove Element.cpp
@@ -21,3 +21,20 @@ public:
         return n;
     }
 };
+
+
+//Using erase()
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int count = 0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i] == val){
+                count++;
+                nums.erase(nums.begin() + i);
+                i--;
+            }
+        }
+        return nums.size();
+    }
+};
