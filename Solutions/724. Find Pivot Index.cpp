1bef664c63dd542e08f424f8877bdbce29ae7a97
Dev Wadhwa <73272997+devwadhwa01@users.noreply.github.com>
2022-01-25 01:28:05 +0530

    Create 724. Find Pivot Index.cpp

---
+++ Solutions/724. Find Pivot Index.cpp
@@ -0,0 +1,27 @@
+//Dev Wadhwa
+
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int n=nums.size();
+        int sum=accumulate(nums.begin(),nums.end(),0);
+        int lsum=0;
+        vector<int> leftsums;
+        vector<int> sumexcept;
+        leftsums.push_back(0);
+        for(int i=1;i<n;i++)
+        {
+            lsum+=nums[i-1];
+            leftsums.push_back(lsum);
+        }
+        for(int i=0;i<n;i++)
+        {
+            sumexcept.push_back(sum-nums[i]);
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(2*leftsums[i]==sumexcept[i]) return i;
+        }
+        return -1;
+    }
+};
