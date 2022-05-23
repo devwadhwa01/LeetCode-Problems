1d3d886a1611fef3af2d11be6cc4343839d450dc
Dev Wadhwa <73272997+devwadhwa01@users.noreply.github.com>
2022-05-23 14:35:09 +0530

    Update 595. Big Countries.sql

--- Solutions/595. Big Countries.sql
+++ Solutions/595. Big Countries.sql
@@ -1,3 +1,3 @@
 //Dev Wadhwa
 
-SELECT name, population, area FROM World WHERE area>3000000 OR population>25000000;
+SELECT name, population, area FROM World WHERE area>=3000000 OR population>=25000000;
