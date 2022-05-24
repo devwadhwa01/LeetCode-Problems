543e6dd75e9ee11c2072269a0508b4c89c5d52aa
Dev Wadhwa <73272997+devwadhwa01@users.noreply.github.com>
2022-05-24 14:54:13 +0530

    Update 20. Valid Parentheses.cpp

--- Solutions/20. Valid Parentheses.cpp
+++ Solutions/20. Valid Parentheses.cpp
@@ -22,3 +22,30 @@ public:
         return (arr.empty()==true);
     }
 };
+
+
+
+//Second Solution (Similar but did not use an extra function)
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack <char> stk;
+        int n=s.size();
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]=='(' || s[i]=='{' || s[i]=='[')
+                stk.push(s[i]);
+            else
+            {
+                if(stk.empty())
+                    return false;
+                else if(((stk.top()=='(' && s[i]==')') || (stk.top()=='{' && s[i]=='}') || (stk.top()=='[' && s[i]==']'))==false)
+                    return false;
+                else
+                    stk.pop();
+            }
+        }
+        return (stk.empty()==true);
+    }
+};
